cdca6e7b236348e2ed44ab6549caf7c20cc78d66
kimmingyu94 <kmg3592@naver.com>
2024-12-27 15:07:50 +0900

    백준 문제

---
+++ src/백준/MinCostClimbingStairsDP문제.java
@@ -0,0 +1,43 @@
+package 백준;
+
+import java.util.HashMap;
+
+public class MinCostClimbingStairsDP문제 {
+
+    //MinCostClimbingStairs DP문제
+    static int[] cost = {10,15,20};
+    static int[] memo = new int[cost.length + 10];
+
+    //topDown 방식
+    public static int topDown(int stair){
+
+        //1층과 2층은 0의 값으로 올라갈 수 있음. * 해당 층의 값이 0이라는게 아님.
+        if(stair == 0 || stair == 1) return 0;
+
+        //메모리에 값이 있으면 해당 값을 반환
+        if(memo[stair] != 0) return memo[stair];
+
+        //해당 층에 올라가기 위한 값 + 해당 층의 값
+        memo[stair] = Math.min(topDown(stair-2) + cost[stair-2] , topDown(stair-1) + cost[stair-1]);
+        return memo[stair];
+    }
+
+    static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    //bottomUp 방식
+    public static int bottomUp(int stairs){
+
+        for(int i=2; i<=stairs; i++){
+            map.put(i, Math.min(map.get(i-1) + cost[i-1], map.get(i-2) + cost[i-2]));
+        }
+        return map.get(stairs);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(topDown(cost.length));
+
+        map.put(0, 0);
+        map.put(1, 0);
+        System.out.println(bottomUp(cost.length));
+    }
+}
---
+++ src/백준/UniquePaths.java
@@ -0,0 +1,62 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class UniquePaths {
+    static int m, n;
+    static int[][] memo;
+
+    static int topDown(int row, int col){
+        if(row == 0 && col == 0){
+            memo[row][col] = 1;
+            return memo[row][col];
+        }
+
+        if(memo[row][col] != 0){
+            return memo[row][col];
+        }
+
+        int left=0;
+        int top=0;
+        if(row > 0) left = topDown(row-1, col);
+        if(col > 0) top = topDown(row, col-1);
+
+        memo[row][col] = left + top;
+        return memo[row][col];
+    }
+
+    static void bottomUp(int row, int col){
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=col; j++){
+                memo[i][j] = memo[i-1][j] + memo[i][j-1];
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        //topDown
+        memo = new int[101][101];
+        System.out.println(topDown(m-1, n-1));
+
+        //bottomUp
+        memo = new int[101][101];
+        for(int i = 0; i < m; i++){
+            memo[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++){
+            memo[0][i] = 1;
+        }
+        bottomUp(m-1, n-1);
+        System.out.println(memo[m-1][n-1]);
+
+        br.close();
+    }
+}
