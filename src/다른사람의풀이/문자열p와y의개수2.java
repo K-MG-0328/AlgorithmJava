c818d8bed875416aff252d7b9c60c1e8604a4ef8
kimmingyu94 <kmg3592@naver.com>
2025-01-05 23:00:23 +0900

    삭제

--- src/다른사람의풀이/문자열p와y의개수2.java
+++
@@ -1,8 +0,0 @@
-package 다른사람의풀이;
-
-public class 문자열p와y의개수2 {
-    boolean solution(String s) {
-        s = s.toUpperCase();
-        return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
-    }
-}
--- src/다른사람의풀이/평균구하기2.java
+++
@@ -1,9 +0,0 @@
-package 다른사람의풀이;
-
-import java.util.Arrays;
-
-public class 평균구하기2 {
-    public double solution(int[] arr) {
-        return Arrays.stream(arr).average().getAsDouble();
-    }
-}
